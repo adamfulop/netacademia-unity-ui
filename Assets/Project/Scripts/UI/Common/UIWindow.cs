49fbcd26b9a5dad49bfa7e8ab4051c44400705a0
Adam Fulop <adam.fulop93@gmail.com>
2018-07-15 15:33:35 +0200

    settings screen done

---
+++ Assets/Project/Scripts/UI/Common/UIWindow.cs
@@ -0,0 +1,16 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour {
+    [SerializeField] private float _tweenDurationSeconds;
+
+    private void Start() {
+        transform.localScale = Vector3.zero;    // kezdetben scale 0
+    }
+
+    public void Show() {
+        transform
+            .DOScale(1, _tweenDurationSeconds)    // scale-t 1-re animáljuk _tweenDurationSeconds másodperc alatt
+            .SetEase(Ease.OutBounce);            // animáció legyen "pattogós"
+    }
+}
---
+++ Assets/Project/Scripts/UI/Settings/SettingsTransition.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsTransition : MonoBehaviour {
+    [SerializeField] private Image _backgroundImage;
+    [SerializeField] private float _tweenDurationSeconds;
+    [SerializeField] private UIWindow _window;
+
+    private void Start() {
+        _backgroundImage.fillAmount = 0;    // kezdetben fill 0
+
+        _backgroundImage
+            .DOFillAmount(1, _tweenDurationSeconds)    // fill 1-re animálása _tweenDurationSeconds másodperc alatt
+            .SetEase(Ease.InOutCubic)                // lassabban indul és lassabban fejeződik be az animáció
+            .OnComplete(_window.Show);                // az animáció után meghívjuk a _window.Show() metódust
+    }
+}
